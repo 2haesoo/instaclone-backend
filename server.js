c36b2bcd3e942f682cf6a8824ce97d7efa21ebf0
2haesoo <gotn676@naver.com>
2021-06-25 17:01:43 -0400

    Test simple api

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-    type Query {
-        hello: String
-    }
+  type Movie {
+    title: String
+    year: Int
+  }
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
 `;
 
 const resolvers = {
-    Query: {
-        hello: () => "world",
-    }
-}
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, args) => {
+        console.log(args);
+        return true
+    },
+    deleteMovie: (_, args) => {
+        console.log(args)
+        return false;
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
-server.listen().then(() => console.log("Server is runnning on http://localhost:4000"))
+server
+  .listen()
+  .then(() => console.log("Server is runnning on http://localhost:4000"));
