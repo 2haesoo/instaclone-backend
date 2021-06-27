1defcfbe905de46f9445a30f1ec209af5cdfe21b
2haesoo <gotn676@naver.com>
2021-06-26 23:35:14 -0400

    Eject from apollo server

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectedResolver } from "./users/users.utils";
+
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    return { loggedInUser: await getUser(req.headers.token), protectedResolver };
+    return {
+      loggedInUser: await getUser(req.headers.token),
+      protectedResolver,
+    };
   },
 });
 
-const PORT = process.env.PORT;
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
 
-server
-  .listen(PORT)
-  .then(() => console.log(`Server is runnning on http://localhost:${PORT}`));
+app.listen({ port: PORT }, () => {
+  console.log(`Server is running on http://localhost:${PORT}/graphql`);
+});
