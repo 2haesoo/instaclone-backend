cc5aec4c1786e74ca85ba7397136d1a08c828ac5
2haesoo <gotn676@naver.com>
2021-06-27 22:53:33 -0400

    Add message subscription

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import http from 'http';
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
@@ -11,18 +12,23 @@ const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-      protectedResolver,
-    };
+    if(req){
+      return {
+        loggedInUser: await getUser(req.headers.token),
+        protectedResolver,
+      };
+    }
   },
 });
 
 const app = express();
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
-apollo.installSubscriptionHandlers(app);
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/graphql`);
 });
