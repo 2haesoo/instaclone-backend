b566e63dd8c4aa242a61da69e5d5a891942c8fc2
2haesoo <gotn676@naver.com>
2021-06-27 23:12:43 -0400

    Add conditional error for subscription 1

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,17 +1,25 @@
 import { withFilter } from "apollo-server-express";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
-    Subscription: {
-        roomUpdates: {
-            subscribe: withFilter(
-                () => pubsub.asyncIterator(NEW_MESSAGE),
-                // (payload, variables) => return true (true 일 경우에만 subscribe)
-                ({ roomUpdates }, { id }) => {
-                    return roomUpdates.roomId === id;
-                }
-            )
-        }
-    }
-}
+  Subscription: {
+    roomUpdates: {
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: { id: args.id },
+          select: { id: true },
+        });
+        if (!room) return new Error("You shall not see this.");
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          // (payload, variables) => return true (true 일 경우에만 subscribe)
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(root, args, context, info);
+      },
+    },
+  },
+};
