12ad6308b67eac971d1880b61b032b5681d1b407
2haesoo <gotn676@naver.com>
2021-06-27 23:02:55 -0400

    Add message subscription with Filter

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,17 @@
+import { withFilter } from "apollo-server-express";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
     Subscription: {
         roomUpdates: {
-            subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+            subscribe: withFilter(
+                () => pubsub.asyncIterator(NEW_MESSAGE),
+                // (payload, variables) => return true (true 일 경우에만 subscribe)
+                ({ roomUpdates }, { id }) => {
+                    return roomUpdates.roomId === id;
+                }
+            )
         }
     }
 }
